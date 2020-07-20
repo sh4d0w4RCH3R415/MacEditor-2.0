d5ed2cf98c663ce943bf88fd69b1da4495097027
sh4d0w4RCH3R415 <46385097+sh4d0w4RCH3R415@users.noreply.github.com>
2020-07-20 14:21:43 -0700

    Added source code to repository

---
+++ Just Editor/MainForm.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Just_Editor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.newFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveFile_ = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileAs_ = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.language = new System.Windows.Forms.ToolStripComboBox();
+            this.wordWrap = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabIndentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabIndent = new System.Windows.Forms.ToolStripComboBox();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileDir = new System.Windows.Forms.ToolStripMenuItem();
+            this.font = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileType = new System.Windows.Forms.ToolStripMenuItem();
+            this.indent = new System.Windows.Forms.ToolStripMenuItem();
+            this.editor = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Window;
+            this.menuStrip1.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.formatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(547, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFile,
+            this.toolStripMenuItem1,
+            this.newFile,
+            this.toolStripMenuItem2,
+            this.saveFile_,
+            this.saveFileAs_});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFile
+            // 
+            this.openFile.Name = "openFile";
+            this.openFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFile.Size = new System.Drawing.Size(238, 22);
+            this.openFile.Text = "Open File";
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(150, 6);
+            // 
+            // newFile
+            // 
+            this.newFile.Name = "newFile";
+            this.newFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newFile.Size = new System.Drawing.Size(238, 22);
+            this.newFile.Text = "New File";
+            this.newFile.Click += new System.EventHandler(this.newFile_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(150, 6);
+            // 
+            // saveFile_
+            // 
+            this.saveFile_.Name = "saveFile_";
+            this.saveFile_.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveFile_.Size = new System.Drawing.Size(238, 22);
+            this.saveFile_.Text = "Save File";
+            this.saveFile_.Click += new System.EventHandler(this.saveFile__Click);
+            // 
+            // saveFileAs_
+            // 
+            this.saveFileAs_.Name = "saveFileAs_";
+            this.saveFileAs_.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveFileAs_.Size = new System.Drawing.Size(238, 22);
+            this.saveFileAs_.Text = "Save File As";
+            this.saveFileAs_.Click += new System.EventHandler(this.saveFileAs__Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem,
+            this.wordWrap,
+            this.tabIndentToolStripMenuItem});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(66, 21);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // languageToolStripMenuItem
+            // 
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.language});
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.languageToolStripMenuItem.Text = "Language";
+            // 
+            // language
+            // 
+            this.language.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.language.Font = new System.Drawing.Font("Roboto", 12F);
+            this.language.Items.AddRange(new object[] {
+            "CSharp",
+            "Custom",
+            "HTML",
+            "JS",
+            "Lua",
+            "PHP",
+            "SQL",
+            "VB",
+            "XML"});
+            this.language.Name = "language";
+            this.language.Size = new System.Drawing.Size(121, 27);
+            this.language.TextChanged += new System.EventHandler(this.language_TextChanged);
+            // 
+            // wordWrap
+            // 
+            this.wordWrap.Checked = true;
+            this.wordWrap.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.wordWrap.Name = "wordWrap";
+            this.wordWrap.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.wordWrap.Size = new System.Drawing.Size(196, 22);
+            this.wordWrap.Text = "Word Wrap";
+            this.wordWrap.Click += new System.EventHandler(this.wordWrap_Click);
+            // 
+            // tabIndentToolStripMenuItem
+            // 
+            this.tabIndentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tabIndent});
+            this.tabIndentToolStripMenuItem.Name = "tabIndentToolStripMenuItem";
+            this.tabIndentToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.tabIndentToolStripMenuItem.Text = "Tab Indent";
+            // 
+            // tabIndent
+            // 
+            this.tabIndent.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tabIndent.Items.AddRange(new object[] {
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.tabIndent.Name = "tabIndent";
+            this.tabIndent.Size = new System.Drawing.Size(121, 23);
+            this.tabIndent.TextChanged += new System.EventHandler(this.tabIndent_TextChanged);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.SystemColors.Window;
+            this.menuStrip2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menuStrip2.Font = new System.Drawing.Font("Roboto", 9F);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileDir,
+            this.font,
+            this.fileType,
+            this.indent});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 426);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(547, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileDir
+            // 
+            this.fileDir.Name = "fileDir";
+            this.fileDir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.fileDir.Size = new System.Drawing.Size(52, 20);
+            this.fileDir.Text = "-none-";
+            this.fileDir.Click += new System.EventHandler(this.fileDir_Click);
+            // 
+            // font
+            // 
+            this.font.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.font.Name = "font";
+            this.font.Size = new System.Drawing.Size(110, 20);
+            this.font.Text = "Consolas, 9.75pt";
+            this.font.Click += new System.EventHandler(this.font_Click);
+            // 
+            // fileType
+            // 
+            this.fileType.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.fileType.Name = "fileType";
+            this.fileType.Size = new System.Drawing.Size(50, 20);
+            this.fileType.Text = "file.cs";
+            // 
+            // indent
+            // 
+            this.indent.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.indent.Name = "indent";
+            this.indent.Size = new System.Drawing.Size(66, 20);
+            this.indent.Text = "Indent: 4";
+            // 
+            // editor
+            // 
+            this.editor.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.editor.AutoIndentCharsPatterns = "\r\n^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;]+);\r\n^\\s*(case|default)\\s*[^:]" +
+    "*(?<range>:)\\s*(?<range>[^;]+);\r\n";
+            this.editor.AutoScrollMinSize = new System.Drawing.Size(0, 15);
+            this.editor.BackBrush = null;
+            this.editor.BracketsHighlightStrategy = FastColoredTextBoxNS.BracketsHighlightStrategy.Strategy2;
+            this.editor.CaretBlinking = false;
+            this.editor.CharHeight = 15;
+            this.editor.CharWidth = 7;
+            this.editor.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.editor.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.editor.Font = new System.Drawing.Font("Consolas", 9.75F);
+            this.editor.IsReplaceMode = false;
+            this.editor.Language = FastColoredTextBoxNS.Language.CSharp;
+            this.editor.LeftBracket = '(';
+            this.editor.LeftBracket2 = '{';
+            this.editor.Location = new System.Drawing.Point(0, 25);
+            this.editor.Name = "editor";
+            this.editor.Paddings = new System.Windows.Forms.Padding(0);
+            this.editor.RightBracket = ')';
+            this.editor.RightBracket2 = '}';
+            this.editor.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.editor.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("editor.ServiceColors")));
+            this.editor.ShowCaretWhenInactive = true;
+            this.editor.Size = new System.Drawing.Size(547, 401);
+            this.editor.TabIndex = 2;
+            this.editor.WordWrap = true;
+            this.editor.Zoom = 100;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(547, 450);
+            this.Controls.Add(this.editor);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "MacEditor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.editor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newFile;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem saveFile_;
+        private System.Windows.Forms.ToolStripMenuItem saveFileAs_;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox language;
+        private System.Windows.Forms.ToolStripMenuItem wordWrap;
+        private System.Windows.Forms.ToolStripMenuItem tabIndentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox tabIndent;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileDir;
+        private System.Windows.Forms.ToolStripMenuItem font;
+        private FastColoredTextBoxNS.FastColoredTextBox editor;
+        private System.Windows.Forms.ToolStripMenuItem fileType;
+        private System.Windows.Forms.ToolStripMenuItem indent;
+    }
+}
+
---
+++ Just Editor/MainForm.cs
@@ -0,0 +1,281 @@
+﻿using FastColoredTextBoxNS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Just_Editor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            if (File.Exists(Application.StartupPath + "\\kernel32.dll"))
+            {
+                InitializeComponent();
+                Timer timer = new Timer();
+                timer.Interval = 1;
+                language.Text = "CSharp";
+                tabIndent.Text = "4";
+                timer.Tick += delegate (object sender, EventArgs e)
+                {
+                    language_TextChanged(sender, e);
+                    tabIndent_TextChanged(sender, e);
+                    font.Text = editor.Font.Name + ", " + editor.Font.Size + "pt";
+                };
+                timer.Start();
+            }
+            else if (!File.Exists(Application.StartupPath + "\\kernel32.dll"))
+            {
+                MessageBox.Show(
+                    "Failed to load application, do you have the required components:\n\n" +
+                    " - kernel32.dll\n\n" +
+                    "If these components are not in the SAME EXACT directory as the application,\n" +
+                    "it will fail to load."
+                    );
+                Close();
+            }
+        }
+
+        private string File_ = "-none-";
+        private string FileSaveType = ".file";
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Title = "Open File",
+                FileName = "file" + FileSaveType,
+                Filter = "All Files|*.*",
+                FilterIndex = 0
+            })
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    using (StreamReader r = new StreamReader(ofd.FileName))
+                    {
+                        editor.Text = r.ReadToEnd();
+                        fileDir.Text = ofd.FileName;
+                        File_ = fileDir.Text;
+                        r.Close();
+                    }
+        }
+        private void newFile_Click(object sender, EventArgs e)
+        {
+            if (editor.Text == "")
+            {
+                File_ = "-none-"; fileDir.Text = "-none-";
+            }
+            else if (editor.Text != "")
+            {
+                var q = MessageBox.Show(
+                    "Warning!\n\n" +
+                    "You have unsaved changes in the editor.\n" +
+                    "Would you like the save them in a file before starting from scratch?\n\n" +
+                    " - [ Yes: Save First ] - [ No: Start From Scratch ] - ",
+                    "",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                    );
+                if (q == DialogResult.Yes)
+                {
+                    using (SaveFileDialog sfd = new SaveFileDialog()
+                    {
+                        Title = "Save File",
+                        FileName = "file" + FileSaveType,
+                        Filter = "All Files|*.*",
+                        FilterIndex = 0
+                    })
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                            using (StreamWriter w = new StreamWriter(sfd.FileName))
+                            {
+                                w.Write(editor.Text);
+                                File_ = sfd.FileName;
+                                fileDir.Text = File_;
+                                w.Close();
+                            }
+                }
+                else if (q == DialogResult.No)
+                    editor.Text = ""; fileDir.Text = "-none-"; File_ = "-none-";
+            }
+        }
+        private void saveFile__Click(object sender, EventArgs e)
+        {
+            if (fileDir.Text == "-none-" & File_ == fileDir.Text & editor.Text != "")
+            {
+                using (SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    Title = "Save File",
+                    FileName = "file" + FileSaveType,
+                    Filter = "All Files|*.*",
+                    FilterIndex = 0
+                })
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                        using (StreamWriter w = new StreamWriter(sfd.FileName))
+                        {
+                            w.Write(editor.Text);
+                            File_ = sfd.FileName;
+                            fileDir.Text = File_;
+                            w.Close();
+                        }
+            } else if (fileDir.Text != "-none-" & File_ == fileDir.Text & editor.Text != "")
+            {
+                using (StreamWriter w = new StreamWriter(File_))
+                {
+                    w.Write(editor.Text);
+                    w.Close();
+                }
+            } else if (fileDir.Text != "-none-" & File_ == fileDir.Text & editor.Text == "")
+            {
+                using (StreamWriter w = new StreamWriter(File_))
+                {
+                    w.Write(editor.Text);
+                    w.Close();
+                }
+            } else if (fileDir.Text == "-none-" & File_ == fileDir.Text & editor.Text == "")
+            {
+                using (SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    Title = "Save Blank File",
+                    FileName = "file.blank",
+                    Filter = "Blank File|*.blank; *.empty",
+                    FilterIndex = 0
+                })
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                        using (StreamWriter w = new StreamWriter(sfd.FileName))
+                        {
+                            w.Write(editor.Text);
+                            File_ = sfd.FileName;
+                            fileDir.Text = File_;
+                            w.Close();
+                        }
+            }
+        }
+        private void saveFileAs__Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog()
+            {
+                Title = "Save File As",
+                FileName = "file" + FileSaveType,
+                Filter = "All Files|*.*",
+                FilterIndex = 0
+            })
+                if (sfd.ShowDialog() == DialogResult.OK)
+                    using (StreamWriter w = new StreamWriter(sfd.FileName))
+                    {
+                        w.Write(editor.Text);
+                        File_ = sfd.FileName;
+                        fileDir.Text = File_;
+                        w.Close();
+                    }
+        }
+        private void wordWrap_Click(object sender, EventArgs e)
+        {
+            wordWrap.Checked = !wordWrap.Checked;
+            editor.WordWrap = wordWrap.Checked;
+        }
+        private void tabIndent_TextChanged(object sender, EventArgs e)
+        {
+            editor.TabLength = Convert.ToInt32(tabIndent.Text);
+            indent.Text = "Indent: " + editor.TabLength;
+        }
+        private void language_TextChanged(object sender, EventArgs e)
+        {
+            switch (language.Text)
+            {
+                case "CSharp":
+                    editor.Language = Language.CSharp;
+                    FileSaveType = ".cs";
+                    fileType.Text = "file.cs";
+                    break;
+
+                case "Custom":
+                    editor.Language = Language.Custom;
+                    FileSaveType = ".*";
+                    fileType.Text = "file.*";
+                    break;
+
+                case "HTML":
+                    editor.Language = Language.HTML;
+                    FileSaveType = ".html";
+                    fileType.Text = "file.html";
+                    break;
+
+                case "JS":
+                    editor.Language = Language.JS;
+                    FileSaveType = ".js";
+                    fileType.Text = "file.js";
+                    break;
+
+                case "Lua":
+                    editor.Language = Language.Lua;
+                    FileSaveType = ".lua";
+                    fileType.Text = "file.lua";
+                    break;
+
+                case "PHP":
+                    editor.Language = Language.PHP;
+                    FileSaveType = ".php";
+                    fileType.Text = "file.php";
+                    break;
+
+                case "SQL":
+                    editor.Language = Language.SQL;
+                    FileSaveType = ".sql";
+                    fileType.Text = "file.sql";
+                    break;
+
+                case "VB":
+                    editor.Language = Language.VB;
+                    FileSaveType = ".vb";
+                    fileType.Text = "file.vb";
+                    break;
+
+                case "XML":
+                    editor.Language = Language.XML;
+                    FileSaveType = ".xml";
+                    fileType.Text = "file.xml";
+                    break;
+            }
+        }
+        private void font_Click(object sender, EventArgs e)
+        {
+            using (FontDialog fd = new FontDialog()
+            {
+                AllowScriptChange = false,
+                AllowSimulations = true,
+                AllowVectorFonts = true,
+                AllowVerticalFonts = false,
+                ShowApply = true,
+                Color = editor.ForeColor,
+                FontMustExist = true,
+                Font = new Font("Consolas", 9.75F),
+                MaxSize = 12,
+                MinSize = 8,
+                FixedPitchOnly = true,
+                ScriptsOnly = false,
+                ShowColor = false,
+                ShowEffects = false
+            })
+            {
+                if (fd.ShowDialog() == DialogResult.OK)
+                    editor.Font = fd.Font;
+                fd.Apply += delegate (object sender_, EventArgs e_)
+                { editor.Font = fd.Font; };
+            }
+        }
+        private void fileDir_Click(object sender, EventArgs e)
+        {
+            if (fileDir.Text != "-none-")
+                Process.Start(new ProcessStartInfo("explorer.exe", Path.GetDirectoryName(File_)));
+            else if (fileDir.Text == "-none-")
+                Process.Start(new ProcessStartInfo("explorer.exe", "C:\\"));
+        }
+    }
+}
